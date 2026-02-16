5b8a3d226be99f65b50b66411f25ba1c007ea815
lysbrodya <lysbrodya@gmail.com>
2026-02-16 12:34:19 +0200

    Updates

--- assets/main-CGmopkOC.js
+++ assets/main-CGmopkOC.js
@@ -30,20 +30,20 @@
   <div class="mobile-menu">
     <button type="button" class="mobile-menu-close-button">
       <svg class="menu-close" width="18" height="18">
-        <use href="svg/icon-close.svg"></use>
+        <use href="./svg/icon-close.svg"></use>
       </svg>
     </button>
 
     <nav class="mobile__nav">
       <ul class="mobile__nav__list">
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link active" href="src/pages/courses/courses.html">КУРСИ</a>
+          <a class="mobile__nav__link active" href="../../pages/courses/courses.html">КУРСИ</a>
         </li>
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link" href="src/pages/blog/blog.html">БЛОГ</a>
+          <a class="mobile__nav__link" href="../../pages/blog/blog.html">БЛОГ</a>
         </li>
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link" href="src/pages/about.html">ПРО ПРОЕКТ</a>
+          <a class="mobile__nav__link" href="../../pages/about.html">ПРО ПРОЕКТ</a>
         </li>
       </ul>
     </nav>
