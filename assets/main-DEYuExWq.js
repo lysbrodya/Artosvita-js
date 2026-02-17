293a406f0060c58de453af21d9181b5b38264a69
lysbrodya <lysbrodya@gmail.com>
2026-02-17 17:43:55 +0200

    Updates

--- assets/main-DEYuExWq.js
+++ assets/main-DEYuExWq.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const a of s)if(a.type==="childList")for(const c of a.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function n(s){const a={};return s.integrity&&(a.integrity=s.integrity),s.referrerPolicy&&(a.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?a.credentials="include":s.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(s){if(s.ep)return;s.ep=!0;const a=n(s);fetch(s.href,a)}})();const r=location.hostname.includes("github.io")?"/Artosvita-js":"";function $(){const e=document.getElementById("header");if(!e)return;const t=e.dataset.button==="true",n=e.dataset.courses==="true";e.innerHTML=`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const a of s)if(a.type==="childList")for(const d of a.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&o(d)}).observe(document,{childList:!0,subtree:!0});function n(s){const a={};return s.integrity&&(a.integrity=s.integrity),s.referrerPolicy&&(a.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?a.credentials="include":s.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(s){if(s.ep)return;s.ep=!0;const a=n(s);fetch(s.href,a)}})();const r=location.hostname.includes("github.io")?"/Artosvita-js":"";function k(){const e=document.getElementById("header");if(!e)return;const t=e.dataset.button==="true",n=e.dataset.courses==="true";e.innerHTML=`
     <div class="header__container container">
         <a class="logo" href="${r}/index.html">
         <div class="logo-div">          
@@ -25,38 +25,38 @@
         </div>
       </button>
       </div>
-  `}const g=location.hostname.includes("github.io")?"/Artosvita-js":"",k=`
+  `}const h=location.hostname.includes("github.io")?"/Artosvita-js":"",E=`
 <div class="backdrop is-hidden">
   <div class="mobile-menu">
     <button type="button" class="mobile-menu-close-button">
       <svg class="menu-close" width="18" height="18">
-        <use href="${g}/svg/icon-close.svg"></use>
+        <use href="${h}/svg/icon-close.svg"></use>
       </svg>
     </button>
 
     <nav class="mobile__nav">
       <ul class="mobile__nav__list">
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link active" href="${g}/src/pages/courses/courses.html">КУРСИ</a>
+          <a class="mobile__nav__link active" href="${h}/src/pages/courses/courses.html">КУРСИ</a>
         </li>
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link" href="${g}/src/pages/blog/blog.html">БЛОГ</a>
+          <a class="mobile__nav__link" href="${h}/src/pages/blog/blog.html">БЛОГ</a>
         </li>
         <li class="mobile__nav__item">
-          <a class="mobile__nav__link" href="${g}/src/pages/about.html">ПРО ПРОЕКТ</a>
+          <a class="mobile__nav__link" href="${h}/src/pages/about.html">ПРО ПРОЕКТ</a>
         </li>
       </ul>
     </nav>
   </div>
 </div>
-`;function E(){document.body.insertAdjacentHTML("beforeend",k);const e=document.querySelector(".backdrop"),t=document.querySelector(".mob-menu"),n=document.querySelector(".mobile-menu-close-button");if(!e||!t||!n)return;t.addEventListener("click",o),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const m=location.hostname.includes("github.io")?"/Artosvita-js":"";async function A(){return(await fetch(`${m}/reviews.json`)).json()}function S(e){return e.map(t=>`<li class="review-card">
+`;function A(){document.body.insertAdjacentHTML("beforeend",E);const e=document.querySelector(".backdrop"),t=document.querySelector(".mob-menu"),n=document.querySelector(".mobile-menu-close-button");if(!e||!t||!n)return;t.addEventListener("click",o),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const g=location.hostname.includes("github.io")?"/Artosvita-js":"";async function S(){return(await fetch(`${g}/reviews.json`)).json()}function j(e){return e.map(t=>`<li class="review-card">
       <div class="review-card__top">
         <img class="review-card__avatar" 
           srcset="
-            ${m}/images/reviews/${t.image}-96.jpg 1x,
-            ${m}/images/reviews/${t.image}-192.jpg 2x
+            ${g}/images/reviews/${t.image}-96.jpg 1x,
+            ${g}/images/reviews/${t.image}-192.jpg 2x
               "
-         src="${m}/images/reviews/${t.image}-96.jpg" 
+         src="${g}/images/reviews/${t.image}-96.jpg" 
          alt="фото випускника"
          width="96" 
          height="96" 
@@ -77,16 +77,16 @@
         </div>
         <a class="review-card__insta" href="${t.instagram}" target="_blank">
           <svg class="icon-insta" width="40" height="40">
-            <use href="${m}/sprite.svg#instagram"></use>
+            <use href="${g}/sprite.svg#instagram"></use>
           </svg>
         </a>
       </div>
-    </li>`).join("")}async function j(){const e=document.querySelector(".slider-track"),t=document.getElementById("next"),n=document.getElementById("prev");if(document.querySelector(".slider"),!e||!t||!n)return;const o=await A(),s=document.querySelector(".course-name")?.textContent.trim(),a=s?o.filter(l=>l.course===s):o;e.innerHTML=S(a);function c(){return window.innerWidth>=1200?3:window.innerWidth>=768?2:1}const d=e.children;if(!d.length)return;const f=c();if(d.length<=f){t.style.display="none",n.style.display="none";return}const p=d[0].cloneNode(!0),b=d[d.length-1].cloneNode(!0);e.appendChild(p),e.insertBefore(b,e.firstChild);let i=1;function u(l=!0){const y=e.children[0],w=getComputedStyle(e),L=parseInt(w.gap)||0,x=y.offsetWidth+L;e.style.transition=l?"transform 0.4s ease":"none",e.style.transform=`translateX(-${i*x}px)`}u(!1),t.onclick=()=>{i++,u()},n.onclick=()=>{i--,u()};let h=0,v=0;e.addEventListener("touchstart",l=>{h=l.touches[0].clientX}),e.addEventListener("touchend",l=>{v=l.changedTouches[0].clientX,_()});function _(){const l=h-v;Math.abs(l)<50||(l>0?t.click():n.click())}e.addEventListener("transitionend",()=>{i===e.children.length-1&&(i=1,u(!1)),i===0&&(i=e.children.length-2,u(!1))})}function M(){document.querySelectorAll(".module-btn").forEach(e=>{e.addEventListener("click",()=>{e.closest(".curriculum-moduls-item").classList.toggle("open")})})}function B(){const e=document.getElementById("header");e&&window.addEventListener("scroll",()=>{const t=e.offsetHeight;window.scrollY>t?e.classList.add("header--scrolled"):e.classList.remove("header--scrolled")})}const H=location.hostname.includes("github.io")?"/Artosvita-js":"",T=`
+    </li>`).join("")}async function M(){const e=document.querySelector(".slider-track"),t=document.getElementById("next"),n=document.getElementById("prev");if(document.querySelector(".slider"),!e||!t||!n)return;const o=await S(),s=document.querySelector(".course-name")?.textContent.trim(),a=s?o.filter(l=>l.course===s):o;e.innerHTML=j(a);function d(){return window.innerWidth>=1200?3:window.innerWidth>=768?2:1}const u=e.children;if(!u.length)return;const p=d();if(u.length<=p){t.style.display="none",n.style.display="none";return}const b=u[0].cloneNode(!0),_=u[u.length-1].cloneNode(!0);e.appendChild(b),e.insertBefore(_,e.firstChild);let i=1;function m(l=!0){const w=e.children[0],$=getComputedStyle(e),L=parseInt($.gap)||0,x=w.offsetWidth+L;e.style.transition=l?"transform 0.4s ease":"none",e.style.transform=`translateX(-${i*x}px)`}m(!1),t.onclick=()=>{i++,m()},n.onclick=()=>{i--,m()};let v=0,f=0;e.addEventListener("touchstart",l=>{v=l.touches[0].clientX}),e.addEventListener("touchend",l=>{f=l.changedTouches[0].clientX,y()});function y(){const l=v-f;Math.abs(l)<50||(l>0?t.click():n.click())}e.addEventListener("transitionend",()=>{i===e.children.length-1&&(i=1,m(!1)),i===0&&(i=e.children.length-2,m(!1))})}function B(){document.querySelectorAll(".module-btn").forEach(e=>{e.addEventListener("click",()=>{e.closest(".curriculum-moduls-item").classList.toggle("open")})})}function H(){const e=document.getElementById("header");e&&window.addEventListener("scroll",()=>{const t=e.offsetHeight;window.scrollY>t?e.classList.add("header--scrolled"):e.classList.remove("header--scrolled")})}const T=location.hostname.includes("github.io")?"/Artosvita-js":"",O=`
 <div class="modal-menu is-hidden" data-modal>
       <div class="modal">
         <button class="modal-btn" data-modal-close type="button">
           <svg class="modal-icon" width="18" height="18">
-            <use href="${H}/svg/icon-close.svg"></use>
+            <use href="${T}/svg/icon-close.svg"></use>
           </svg>
         </button>
         <p class="form-title">ЗАПИС НА КУРС</p>
@@ -156,15 +156,15 @@
         </form>
       </div>
     </div>
-`;function O(){document.body.insertAdjacentHTML("beforeend",T);const e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),n=document.querySelector("[data-modal-close]");if(!e||!t||!n)return;t.forEach(s=>{s.addEventListener("click",o)}),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}function q(){const e=document.getElementById("tg-bots");e&&(e.innerHTML=`      
+`;function q(){document.body.insertAdjacentHTML("beforeend",O);const e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),n=document.querySelector("[data-modal-close]");if(!e||!t||!n)return;t.forEach(s=>{s.addEventListener("click",o)}),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const c=location.hostname.includes("github.io")?"/Artosvita-js":"";function C(){const e=document.getElementById("tg-bots");e&&(e.innerHTML=`      
     <a href="https://t.me/maryartosvita" target="_blank">
         <img 
             srcset="
-            /images/smole-img/bot-m-80.jpg 1x,
-            /images/smole-img/bot-m-160.jpg 2x
+            ${c}/images/smole-img/bot-m-80.jpg 1x,
+            ${c}/images/smole-img/bot-m-160.jpg 2x
               "
         
-         src="./images/smole-img/bot-m-160.jpg" 
+         src="${c}/images/smole-img/bot-m-160.jpg" 
          alt="бот" 
          width="80" 
          height="80" 
@@ -174,14 +174,14 @@
     <a href="https://t.me/vstup_artosvita" target="_blank">
         <img 
             srcset="
-            /images/smole-img/bot-vstup-80.jpg 1x,
-            /images/smole-img/bot-vstup-160.jpg 2x
+            ${c}/images/smole-img/bot-vstup-80.jpg 1x,
+            ${c}/images/smole-img/bot-vstup-160.jpg 2x
               "
         
-         src="./images/smole-img/bot-vstup-160.jpg" 
+         src="${c}/images/smole-img/bot-vstup-160.jpg" 
          alt="бот" 
          width="80" 
          height="80" 
          loading="lazy"
     />
-      </a>`)}$();j();M();B();q();document.addEventListener("DOMContentLoaded",()=>{E()});document.addEventListener("DOMContentLoaded",()=>{O()});
+      </a>`)}k();M();B();H();C();document.addEventListener("DOMContentLoaded",()=>{A()});document.addEventListener("DOMContentLoaded",()=>{q()});
