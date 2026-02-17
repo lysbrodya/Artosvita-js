2bcafd99d9aac7b89a3d9dd40f3476f3b3cf0fc9
lysbrodya <lysbrodya@gmail.com>
2026-02-17 21:24:04 +0200

    Updates

--- assets/main-BVX-vPwA.js
+++ assets/main-BVX-vPwA.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const a of s)if(a.type==="childList")for(const d of a.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&o(d)}).observe(document,{childList:!0,subtree:!0});function n(s){const a={};return s.integrity&&(a.integrity=s.integrity),s.referrerPolicy&&(a.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?a.credentials="include":s.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(s){if(s.ep)return;s.ep=!0;const a=n(s);fetch(s.href,a)}})();const r=location.hostname.includes("github.io")?"/Artosvita-js":"";function k(){const e=document.getElementById("header");if(!e)return;const t=e.dataset.button==="true",n=e.dataset.courses==="true";e.innerHTML=`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const o of s)if(o.type==="childList")for(const d of o.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&a(d)}).observe(document,{childList:!0,subtree:!0});function n(s){const o={};return s.integrity&&(o.integrity=s.integrity),s.referrerPolicy&&(o.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?o.credentials="include":s.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(s){if(s.ep)return;s.ep=!0;const o=n(s);fetch(s.href,o)}})();const r=location.hostname.includes("github.io")?"/Artosvita-js":"";function k(){const e=document.getElementById("header");if(!e)return;const t=e.dataset.button==="true",n=e.dataset.courses==="true";e.innerHTML=`
     <div class="header__container container">
         <a class="logo" href="${r}/index.html">
         <div class="logo-div">          
@@ -17,7 +17,7 @@
           ${n?"":`<a class="nav-courses" href="${r}/src/pages/courses/courses.html">КУРСИ</a>`}
         </nav>
         ${t?'<button class="sign-up" data-modal-open>ЗАПИСАТИСЯ</button>':""}
-              <button class="mob-menu" type="button">
+              <button class="mob-menu" aria-label="Відкрити меню" type="button">
 <div type="button" class="mob-menu__border">
         <svg class="mob-menu__icon" width="40" height="32">
           <use href="${r}/sprite.svg#icon-menu"></use>
@@ -49,7 +49,7 @@
     </nav>
   </div>
 </div>
-`;function A(){document.body.insertAdjacentHTML("beforeend",E);const e=document.querySelector(".backdrop"),t=document.querySelector(".mob-menu"),n=document.querySelector(".mobile-menu-close-button");if(!e||!t||!n)return;t.addEventListener("click",o),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const g=location.hostname.includes("github.io")?"/Artosvita-js":"";async function S(){return(await fetch(`${g}/reviews.json`)).json()}function j(e){return e.map(t=>`<li class="review-card">
+`;function A(){document.body.insertAdjacentHTML("beforeend",E);const e=document.querySelector(".backdrop"),t=document.querySelector(".mob-menu"),n=document.querySelector(".mobile-menu-close-button");if(!e||!t||!n)return;t.addEventListener("click",a),n.addEventListener("click",a),e.addEventListener("click",s=>{s.target===e&&a()});function a(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const g=location.hostname.includes("github.io")?"/Artosvita-js":"";async function S(){return(await fetch(`${g}/reviews.json`)).json()}function j(e){return e.map(t=>`<li class="review-card">
       <div class="review-card__top">
         <img class="review-card__avatar" 
           srcset="
@@ -75,13 +75,13 @@
           <span class="score-first">БАЛ:</span>
           <span class="review-card__score-value">${t.score}</span>
         </div>
-        <a class="review-card__insta" href="${t.instagram}" target="_blank">
+        <a class="review-card__insta" aria-label="Перейти на сторінку інстаграму" href="${t.instagram}" target="_blank">
           <svg class="icon-insta" width="40" height="40">
             <use href="${g}/sprite.svg#instagram"></use>
           </svg>
         </a>
       </div>
-    </li>`).join("")}async function M(){const e=document.querySelector(".slider-track"),t=document.getElementById("next"),n=document.getElementById("prev");if(document.querySelector(".slider"),!e||!t||!n)return;const o=await S(),s=document.querySelector(".course-name")?.textContent.trim(),a=s?o.filter(l=>l.course===s):o;e.innerHTML=j(a);function d(){return window.innerWidth>=1200?3:window.innerWidth>=768?2:1}const u=e.children;if(!u.length)return;const p=d();if(u.length<=p){t.style.display="none",n.style.display="none";return}const b=u[0].cloneNode(!0),_=u[u.length-1].cloneNode(!0);e.appendChild(b),e.insertBefore(_,e.firstChild);let i=1;function m(l=!0){const w=e.children[0],$=getComputedStyle(e),L=parseInt($.gap)||0,x=w.offsetWidth+L;e.style.transition=l?"transform 0.4s ease":"none",e.style.transform=`translateX(-${i*x}px)`}m(!1),t.onclick=()=>{i++,m()},n.onclick=()=>{i--,m()};let v=0,f=0;e.addEventListener("touchstart",l=>{v=l.touches[0].clientX}),e.addEventListener("touchend",l=>{f=l.changedTouches[0].clientX,y()});function y(){const l=v-f;Math.abs(l)<50||(l>0?t.click():n.click())}e.addEventListener("transitionend",()=>{i===e.children.length-1&&(i=1,m(!1)),i===0&&(i=e.children.length-2,m(!1))})}function B(){document.querySelectorAll(".module-btn").forEach(e=>{e.addEventListener("click",()=>{e.closest(".curriculum-moduls-item").classList.toggle("open")})})}function H(){const e=document.getElementById("header");e&&window.addEventListener("scroll",()=>{const t=e.offsetHeight;window.scrollY>t?e.classList.add("header--scrolled"):e.classList.remove("header--scrolled")})}const T=location.hostname.includes("github.io")?"/Artosvita-js":"",O=`
+    </li>`).join("")}async function M(){const e=document.querySelector(".slider-track"),t=document.getElementById("next"),n=document.getElementById("prev");if(document.querySelector(".slider"),!e||!t||!n)return;const a=await S(),s=document.querySelector(".course-name")?.textContent.trim(),o=s?a.filter(l=>l.course===s):a;e.innerHTML=j(o);function d(){return window.innerWidth>=1200?3:window.innerWidth>=768?2:1}const u=e.children;if(!u.length)return;const p=d();if(u.length<=p){t.style.display="none",n.style.display="none";return}const b=u[0].cloneNode(!0),_=u[u.length-1].cloneNode(!0);e.appendChild(b),e.insertBefore(_,e.firstChild);let i=1;function m(l=!0){const w=e.children[0],$=getComputedStyle(e),L=parseInt($.gap)||0,x=w.offsetWidth+L;e.style.transition=l?"transform 0.4s ease":"none",e.style.transform=`translateX(-${i*x}px)`}m(!1),t.onclick=()=>{i++,m()},n.onclick=()=>{i--,m()};let v=0,f=0;e.addEventListener("touchstart",l=>{v=l.touches[0].clientX}),e.addEventListener("touchend",l=>{f=l.changedTouches[0].clientX,y()});function y(){const l=v-f;Math.abs(l)<50||(l>0?t.click():n.click())}e.addEventListener("transitionend",()=>{i===e.children.length-1&&(i=1,m(!1)),i===0&&(i=e.children.length-2,m(!1))})}function B(){document.querySelectorAll(".module-btn").forEach(e=>{e.addEventListener("click",()=>{e.closest(".curriculum-moduls-item").classList.toggle("open")})})}function H(){const e=document.getElementById("header");e&&window.addEventListener("scroll",()=>{const t=e.offsetHeight;window.scrollY>t?e.classList.add("header--scrolled"):e.classList.remove("header--scrolled")})}const T=location.hostname.includes("github.io")?"/Artosvita-js":"",O=`
 <div class="modal-menu is-hidden" data-modal>
       <div class="modal">
         <button class="modal-btn" data-modal-close type="button">
@@ -156,7 +156,7 @@
         </form>
       </div>
     </div>
-`;function q(){document.body.insertAdjacentHTML("beforeend",O);const e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),n=document.querySelector("[data-modal-close]");if(!e||!t||!n)return;t.forEach(s=>{s.addEventListener("click",o)}),n.addEventListener("click",o),e.addEventListener("click",s=>{s.target===e&&o()});function o(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const c=location.hostname.includes("github.io")?"/Artosvita-js":"";function C(){const e=document.getElementById("tg-bots");e&&(e.innerHTML=`      
+`;function q(){document.body.insertAdjacentHTML("beforeend",O);const e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),n=document.querySelector("[data-modal-close]");if(!e||!t||!n)return;t.forEach(s=>{s.addEventListener("click",a)}),n.addEventListener("click",a),e.addEventListener("click",s=>{s.target===e&&a()});function a(){e.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}}const c=location.hostname.includes("github.io")?"/Artosvita-js":"";function C(){const e=document.getElementById("tg-bots");e&&(e.innerHTML=`      
     <a href="https://t.me/maryartosvita" target="_blank">
         <img 
             srcset="
